b70191a73836fd48c8de1d7f1ca9cafe23cca5ff
ethan-arrowood <ethan.arrowood@gmail.com>
2016-06-06 23:01:53 -0400

    filter column finish on static site

--- static-example.js
+++ static-example.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
-    allFilter("#all_categories", ".categories-list");
-    allFilter("#all_tags", ".tags-list")
-});
-
-function allFilter (all_list, list) {
-    $(all_list).change(function() {
-        console.log("State: ", this.checked)
-        $(list).toggle("fast");
-    })
+function toggleHandler(section) {
+    $(section).toggle("fast");
 }
 
-function clearSearch (searchField) {
-    $("[name=" + searchField + "]").val("");
+function clearField (field) {
+    $(field).val("");
 }
+
+function checkMin(max) {
+    var min = $('[name=min-price]').val();
+    
+    if (min >= max) {
+        $('[name=min-price]').val(parseInt(max) - 1);
+    }
+    if (min == "") {
+        min = 0;
+    }
+    
+}
+
+function checkMax(min) {
+    var max = $('[name=max-price]').val();
+    
+    if (min >= max ) {
+        $('[name=max-price]').val(parseInt(min) + 1);
+    }
+    if (max == "") {
+        max = min + 1;
+    }
+}
+
